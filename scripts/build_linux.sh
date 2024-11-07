138af4703059c355a255eae5654bf215ef26ed97
Matthew Ballance <matt.ballance@gmail.com>
2024-11-07 23:48:52 +0000

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

---
+++ scripts/build_linux.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+cwd=$(pwd)
+
+if test "x${CONTAINER}" != "x"; then
+    yum install -y flex bison gperf autoconf wget
+fi
+
+if test "x${TARGET}" = "x"; then
+    TARGET=linux
+fi
+
+systemc_version=3.0.1
+systemc_tgz=${cwd}/3.0.1.tar.gz
+systemc_dir=${cwd}/systemc-3.0.1
+
+if test ! -f ${systemc_tgz}; then
+    wget https://github.com/accellera-official/systemc/archive/refs/tags/3.0.1.tar.gz
+    if test $? -ne 0; then exit 1; fi
+fi
+
+if test -d ${systemc_dir}; then
+    rm -rf ${systemc_dir}
+fi
+
+tar xvf ${systemc_tgz}
+if test $? -ne 0; then exit 1; fi
+
+cd ${systemc_dir}
+
+# Patch...
+touch docs/DEVELOPMENT.md
+
+#autoconf
+#if test $? -ne 0; then exit 1; fi
+
+./configure --prefix=${cwd}/systemc-${TARGET}-${systemc_version}
+if test $? -ne 0; then exit 1; fi
+
+make -j$(nproc)
+if test $? -ne 0; then exit 1; fi
+
+make install
+if test $? -ne 0; then exit 1; fi
+
+cd ${cwd}
+
+tar czf systemc-${TARGET}-${systemc_version}.tar.gz systemc-${TARGET}-${systemc_version}
+if test $? -ne 0; then exit 1; fi
+
+pwd
+ls
